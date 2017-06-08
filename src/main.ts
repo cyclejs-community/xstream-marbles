90196a07e9157f96a6c577edce078ae52bc7535c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-09 01:53:07 +0530

    Allow more debugging

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,28 @@ import { Stream } from 'xstream';
 import { operators } from './data/operators';
 import fromDiagram from 'xstream/extra/fromDiagram';
 
+const noop = () => { };
+const dummy = {
+  next: noop,
+  error: noop,
+  complete: noop
+};
+
 function main(sources: ISources): ISinks {
   const xs = Stream;
-  sources.routes.route$.addListener({
-    next: route => console.log('route: ' + route),
-    error: () => { },
-    complete: () => { }
-  });
+  sources.routes.route$.addListener(dummy);
   sources.data.data$.addListener({
     next: example => {
       console.log(example);
       console.log(example.inputs[0]);
-      example.operate(fromDiagram(example.inputs[0].value, example.inputs[0].options))[0].addListener({
-        next: ev => console.log(ev),
-        error: () => { },
-        complete: () => { }
-      });
+      const input$: Stream<string> = fromDiagram(example.inputs[0].value, example.inputs[0].options);
+      const output$ = Stream.merge(Stream.of('_FIRST_'), example.operate(input$)[0]);
+      const periodic$ = Stream.periodic(20).take(100);
+      Stream.combine(periodic$, output$)
+        .fold(({ value, last }, [period, output]) => ({ value: value + (output === last ? '-' : output), last: output }), { value: '', last: '_FIRST_' })
+        .map(result => result.value)
+        .debug()
+        .addListener(dummy);
     },
     error: () => { },
     complete: () => { }
