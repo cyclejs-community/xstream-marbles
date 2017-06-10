a862c97eef21f7ae059595ada080e2139f0e7700
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-10 12:24:21 +0530

    Add filter

--- src/data/operators.ts
+++ src/data/operators.ts
@@ -4,17 +4,24 @@ import { Marble, OperatorExample } from '../definitions';
 export const examples: IndexedOperatorExamples = {
   'map': {
     inputs: [
-      [ { data: '1', time: 20 }, { data: '3', time: 40 }, { data: '7', time: 60 } ]
+      [ { data: '1', time: 25 }, { data: '3', time: 30 }, { data: '7', time: 50 } ]
     ],
-    label: 'map(x => x * 10)',
+    label: 'map(x => 10 * x)',
     operate: input$ => [input$.map(({ data, time }) => ({ data: (parseInt(data) * 10).toString(), time }))]
   },
   'mapTo': {
     inputs: [
-      [ { data: '1', time: 20 }, { data: '3', time: 40 }, { data: '7', time: 60 } ]
+      [ { data: '1', time: 25 }, { data: '3', time: 30 }, { data: '7', time: 50 } ]
     ],
     label: 'mapTo(10)',
     operate: input$ => [input$.map(({ data, time }) => ({ data: '10', time }))]
+  },
+  'filter': {
+    inputs: [
+      [ { data: '1', time: 20 }, { data: '2', time: 25 }, { data: '1', time: 30 }, { data: '1', time: 45 }, { data: '7', time: 50 }, { data: '4', time: 60 } ]
+    ],
+    label: 'filter(x => x > 1)',
+    operate: input$ => [input$.filter(({ data }) => parseInt(data) > 1)]
   },
 };
 
