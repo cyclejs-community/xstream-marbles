d3d87eea3a5805396287361a8d3d35e5adf35b9f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-10 13:03:52 +0530

    Use helpers for completion

--- src/data/operators.ts
+++ src/data/operators.ts
@@ -1,20 +1,21 @@
 import { Stream } from 'xstream';
 import { Marble, OperatorExample } from '../definitions';
+import { withCompletionMarble$ } from './helpers';
 
 export const examples: IndexedOperatorExamples = {
   'map': {
     inputs: [
-      [{ data: '1', time: 25 }, { data: '3', time: 30 }, { data: '7', time: 50 }]
+      [{ data: '1', time: 25 }, { data: '3', time: 30 }, { data: '7', time: 50 }, { time: 100, complete: true }]
     ],
     label: 'map(x => 10 * x)',
-    operate: input$ => [input$.map(({ data, time }) => ({ data: (parseInt(data) * 10).toString(), time }))]
+    operate: input$ => [input$.map(({ data, time, complete }) => ({ data: data && (parseInt(data) * 10).toString(), time, complete }))]
   },
   'mapTo': {
     inputs: [
-      [{ data: '1', time: 25 }, { data: '3', time: 30 }, { data: '7', time: 50 }]
+      [{ data: '1', time: 25 }, { data: '3', time: 30 }, { data: '7', time: 50 }, { time: 100, complete: true }]
     ],
     label: 'mapTo(10)',
-    operate: input$ => [input$.map(({ data, time }) => ({ data: '10', time }))]
+    operate: input$ => [input$.map(({ data, time, complete }) => ({ data: data && '10', time, complete }))]
   },
   'filter': {
     inputs: [
@@ -28,24 +29,7 @@ export const examples: IndexedOperatorExamples = {
       [{ data: '1', time: 20 }, { data: '2', time: 25 }, { data: '1', time: 30 }, { data: '1', time: 45 }, { data: '7', time: 50 }, { data: '4', time: 60 }]
     ],
     label: 'take(4)',
-    operate: input$ => {
-      const output$ = Stream.create<Marble>();
-      let lastTime = 0;
-      setTimeout(() =>
-        input$.take(4)
-          .addListener({
-            next: marble => {
-              lastTime = marble.time;
-              output$.shamefullySendNext(marble);
-            },
-            complete: () => {
-              output$.shamefullySendNext({ time: lastTime, complete: true, data: '|' });
-              output$.shamefullySendComplete();
-            },
-            error: err => output$.shamefullySendError(err)
-          }), 30);
-      return [output$];
-    }
+    operate: input$ => [withCompletionMarble$(input$.take(4))]
   },
 };
 
