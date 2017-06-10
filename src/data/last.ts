0d60b3813d0ba79be56399e72667d802beb798a2
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-10 13:31:17 +0530

    Refactor out last

---
+++ src/data/last.ts
@@ -0,0 +1,29 @@
+import { OperatorExample } from '../definitions';
+import { withCompletionMarble$ } from './helpers';
+
+export const last = {
+  inputs: [
+    [
+      { data: '1', time: 20 },
+      { data: '2', time: 25 },
+      { data: '1', time: 30 },
+      { data: '1', time: 45 },
+      { data: '7', time: 50 },
+      { data: '4', time: 60 },
+      { time: 100, complete: true }
+    ]
+  ],
+  label: 'last()',
+  operate: input$ => [
+    withCompletionMarble$(
+      input$
+        .last()
+        .map(({ time }) =>
+          input$
+            .filter(({ complete }) => !complete)
+            .last()
+            .map(({ data }) => ({ data, time })))
+        .flatten()
+    )
+  ]
+};
--- src/data/operators.ts
+++ src/data/operators.ts
@@ -6,6 +6,7 @@ import { mapTo } from './mapTo';
 import { filter } from './filter';
 import { take } from './take';
 import { drop } from './drop';
+import { last } from './last';
 
 export const examples: IndexedOperatorExamples = {
   'map': map,
@@ -13,23 +14,7 @@ export const examples: IndexedOperatorExamples = {
   'filter': filter,
   'take': take,
   'drop': drop,
-  'last': {
-    inputs: [
-      [
-        { data: '1', time: 20 },
-        { data: '2', time: 25 },
-        { data: '1', time: 30 },
-        { data: '1', time: 45 },
-        { data: '7', time: 50 },
-        { data: '4', time: 60 },
-        { time: 100, complete: true }
-      ]
-    ],
-    label: 'last()',
-    operate: input$ => [
-      withCompletionMarble$(input$.last().map(({ time }) =>input$.filter(({ complete }) => !complete).last().map(({ data }) => ({ data, time }))).flatten())
-    ]
-  }
+  'last': last
 };
 
 interface IndexedOperatorExamples {
