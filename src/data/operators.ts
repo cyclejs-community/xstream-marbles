38ef6accbf59bf033591af26fa1adbba95fc9ec6
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-10 13:16:41 +0530

    Add last

--- src/data/operators.ts
+++ src/data/operators.ts
@@ -38,6 +38,23 @@ export const examples: IndexedOperatorExamples = {
     label: 'drop(4)',
     operate: input$ => [withCompletionMarble$(input$.drop(4))]
   },
+  'last': {
+    inputs: [
+      [{ data: '1', time: 20 }, { data: '2', time: 25 }, { data: '1', time: 30 }, { data: '1', time: 45 }, { data: '7', time: 50 }, { data: '4', time: 60 }, { time: 100, complete: true }]
+    ],
+    label: 'last()',
+    operate: input$ => [
+      withCompletionMarble$(
+        input$.last()
+          .map(({ time }) =>
+            input$
+              .filter(({ complete }) => !complete)
+              .last()
+              .map(({ data }) => ({ data, time })))
+          .flatten()
+      )
+    ]
+  }
 };
 
 interface IndexedOperatorExamples {
