e86383a0803181153199cbee714fcd0cb28c882a
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-10 13:29:28 +0530

    Refactor out drop

--- src/data/operators.ts
+++ src/data/operators.ts
@@ -5,29 +5,14 @@ import { map } from './map';
 import { mapTo } from './mapTo';
 import { filter } from './filter';
 import { take } from './take';
+import { drop } from './drop';
 
 export const examples: IndexedOperatorExamples = {
   'map': map,
   'mapTo': mapTo,
   'filter': filter,
   'take': take,
-  'drop': {
-    inputs: [
-      [
-        { data: '1', time: 20 },
-        { data: '2', time: 25 },
-        { data: '1', time: 30 },
-        { data: '1', time: 45 },
-        { data: '7', time: 50 },
-        { data: '4', time: 60 },
-        { time: 100, complete: true }
-      ]
-    ],
-    label: 'drop(4)',
-    operate: input$ => [
-      input$.drop(4)
-    ]
-  },
+  'drop': drop,
   'last': {
     inputs: [
       [
