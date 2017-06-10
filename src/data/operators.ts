1842f0609a34adf8800e36d637da89798ab2d5b8
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-10 13:22:06 +0530

    Make code more readable

--- src/data/operators.ts
+++ src/data/operators.ts
@@ -5,54 +5,98 @@ import { withCompletionMarble$ } from './helpers';
 export const examples: IndexedOperatorExamples = {
   'map': {
     inputs: [
-      [{ data: '1', time: 25 }, { data: '3', time: 30 }, { data: '7', time: 50 }, { time: 100, complete: true }]
+      [
+        { data: '1', time: 25 },
+        { data: '3', time: 30 },
+        { data: '7', time: 50 },
+        { time: 100, complete: true }
+      ]
     ],
     label: 'map(x => 10 * x)',
-    operate: input$ => [input$.map(({ data, time, complete }) => ({ data: data && (parseInt(data) * 10).toString(), time, complete }))]
+    operate: input$ => [
+      input$.map(({ data, time, complete }) => ({ data: data && (parseInt(data) * 10).toString(), time, complete }))
+    ]
   },
   'mapTo': {
     inputs: [
-      [{ data: '1', time: 25 }, { data: '3', time: 30 }, { data: '7', time: 50 }, { time: 100, complete: true }]
+      [
+        { data: '1', time: 25 },
+        { data: '3', time: 30 },
+        { data: '7', time: 50 },
+        { time: 100, complete: true }
+      ]
     ],
     label: 'mapTo(10)',
-    operate: input$ => [input$.map(({ data, time, complete }) => ({ data: data && '10', time, complete }))]
+    operate: input$ => [
+      input$.map(({ data, time, complete }) => ({ data: data && '10', time, complete }))
+    ]
   },
   'filter': {
     inputs: [
-      [{ data: '1', time: 20 }, { data: '2', time: 25 }, { data: '1', time: 30 }, { data: '1', time: 45 }, { data: '7', time: 50 }, { data: '4', time: 60 }, { time: 100, complete: true }]
+      [
+        { data: '1', time: 20 },
+        { data: '2', time: 25 },
+        { data: '1', time: 30 },
+        { data: '1', time: 45 },
+        { data: '7', time: 50 },
+        { data: '4', time: 60 },
+        { time: 100, complete: true }
+      ]
     ],
     label: 'filter(x => x > 1)',
-    operate: input$ => [input$.filter(({ data, complete }) => complete != undefined || parseInt(data) > 1)]
+    operate: input$ => [
+      input$.filter(({ data, complete }) => complete != undefined || parseInt(data) > 1)
+    ]
   },
   'take': {
     inputs: [
-      [{ data: '1', time: 20 }, { data: '2', time: 25 }, { data: '1', time: 30 }, { data: '1', time: 45 }, { data: '7', time: 50 }, { data: '4', time: 60 }, { time: 100, complete: true }]
+      [
+        { data: '1', time: 20 },
+        { data: '2', time: 25 },
+        { data: '1', time: 30 },
+        { data: '1', time: 45 },
+        { data: '7', time: 50 },
+        { data: '4', time: 60 },
+        { time: 100, complete: true }
+      ]
     ],
     label: 'take(4)',
-    operate: input$ => [withCompletionMarble$(input$.take(4))]
+    operate: input$ => [
+      withCompletionMarble$(input$.take(4))
+    ]
   },
   'drop': {
     inputs: [
-      [{ data: '1', time: 20 }, { data: '2', time: 25 }, { data: '1', time: 30 }, { data: '1', time: 45 }, { data: '7', time: 50 }, { data: '4', time: 60 }, { time: 100, complete: true }]
+      [
+        { data: '1', time: 20 },
+        { data: '2', time: 25 },
+        { data: '1', time: 30 },
+        { data: '1', time: 45 },
+        { data: '7', time: 50 },
+        { data: '4', time: 60 },
+        { time: 100, complete: true }
+      ]
     ],
     label: 'drop(4)',
-    operate: input$ => [withCompletionMarble$(input$.drop(4))]
+    operate: input$ => [
+      withCompletionMarble$(input$.drop(4))
+    ]
   },
   'last': {
     inputs: [
-      [{ data: '1', time: 20 }, { data: '2', time: 25 }, { data: '1', time: 30 }, { data: '1', time: 45 }, { data: '7', time: 50 }, { data: '4', time: 60 }, { time: 100, complete: true }]
+      [
+        { data: '1', time: 20 },
+        { data: '2', time: 25 },
+        { data: '1', time: 30 },
+        { data: '1', time: 45 },
+        { data: '7', time: 50 },
+        { data: '4', time: 60 },
+        { time: 100, complete: true }
+      ]
     ],
     label: 'last()',
     operate: input$ => [
-      withCompletionMarble$(
-        input$.last()
-          .map(({ time }) =>
-            input$
-              .filter(({ complete }) => !complete)
-              .last()
-              .map(({ data }) => ({ data, time })))
-          .flatten()
-      )
+      withCompletionMarble$(input$.last().map(({ time }) =>input$.filter(({ complete }) => !complete).last().map(({ data }) => ({ data, time }))).flatten())
     ]
   }
 };
